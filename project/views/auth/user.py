767fd69ddc0669c344b71631ff748fbab4281ce1
SaixFix <saixgs@gmail.com>
2022-10-12 16:18:49 +0300

    created user's login views

--- project/views/auth/user.py
+++ project/views/auth/user.py
@@ -1,3 +1,20 @@
 from flask_restx import Namespace
 
 api = Namespace('user')
+
+#todo доделать
+@api.route("/")
+class UserView:
+
+    def get(self):
+        """get user's infomation"""
+
+    def patch(self):
+        """change information user"""
+
+
+@api.route("/password")
+class UserPassView:
+
+    def put(self, password):
+        """get old password and new then change password"""
